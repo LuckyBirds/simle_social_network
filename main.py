8d15ddfcaca49b73522fa25f8b3f0725c8e90871
LuckyBirds <root@go387.localdomain>
2020-12-07 11:18:00 +0300

    add chat

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_mysqldb import MySQL
 from passlib.hash import pbkdf2_sha256
 from flask_security.utils import verify_password
+
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, TextAreaField, SelectField, validators, ValidationError, TextField, TextAreaField, SubmitField, IntegerField, PasswordField, DateField
+from wtforms.validators import DataRequired, Email
+
+import time
+
 import MySQLdb.cursors
 import re
 import passlib
@@ -12,14 +19,31 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.secret_key = 'your secret key'
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'socialuser'
 app.config['MYSQL_PASSWORD'] = 'socialpass'
 app.config['MYSQL_DB'] = 'social'
+app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
+
+
+#import pdb; pdb.set_trace()
+
+class ChatForm(FlaskForm):
+    chat_text = TextAreaField("Chat text", validators=[DataRequired()]) 
+    #friend_id = IntegerField()
+    submit = SubmitField("Отправить новость")
+
+class NewsForm(FlaskForm):
+    news_text = TextAreaField("News text", validators=[DataRequired()]) 
+    submit = SubmitField("Отправить новость")
+
+
+
 
 
 @app.route('/social/', methods=['GET', 'POST'])
@@ -101,16 +125,36 @@ def register():
           msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
 
-@app.route('/social/home')
+@app.route('/social/home', methods=['GET', 'POST'])
 def home():
     if 'loggedin' in session:
+        form = NewsForm()
+        if form.validate_on_submit():
+            #flash('Login requested for user {}'.format(form.news_text.data))
+
+            news_text = form.news_text.data
+      
+            
+           
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('INSERT INTO news ( author_id, news_text, news_date )   VALUES(%s, %s, %s) ', (session['id'], news_text, timestamp  ),)
+            mysql.connection.commit()
+
+            return redirect(url_for('home'))
+        
+        
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
         account = cursor.fetchone()
+        
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts LEFT JOIN friends  ON accounts.id =  friends.friend_id WHERE friends.account_id =  %s', (session['id'],))
         friends = cursor.fetchall()
-        return render_template('home.html', account=account, friends=friends)
+        cursor.execute('SELECT * FROM news WHERE author_id =  %s', (session['id'],))
+        news = cursor.fetchall()
+        return render_template('home.html', account=account, friends=friends, news=news,  form=form)
+        
     return redirect(url_for('login'))
     
 
@@ -163,34 +207,33 @@ def displayman():
 @app.route('/social/displayfriend', methods=['GET', 'POST'])
 def displayfriend():
     if 'loggedin' in session:
-        if request.method == 'GET': 
-            friend_id = request.args.get('friend_id') 
-            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute('SELECT * FROM accounts where id = %s',   [friend_id])
-            account = cursor.fetchone()
-            cursor.execute('SELECT user1.name, user2.name,  message_text, message_date FROM dialogs   \
-            left join accounts as user1 on dialogs.message_from  = user1.id \
-            left join accounts as user2 on dialogs.message_to  = user2.id \
-            where  dialogs.message_from = %s or dialogs.message_to = %s  ;',   (session['id'],session['id']))
-            
-            dialog = cursor.fetchall()
-            return render_template('displayfriend.html', account=account, dialog=dialog)
-        if request.method == 'POST':
-            friend_id = request.form.get('friend_id')
-            chat_text = request.form.get('chat_text')
+        form = ChatForm()
+
+        if form.validate_on_submit():
+            #flash('Login requested for user {}'.format(form.news_text.data))
+
+            chat_text = form.chat_text.data
+            frind_id = form.friend_id.data
+                       
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             cursor.execute('INSERT INTO dialogs (  message_from, message_to ,message_text ,message_date )   VALUES(%s, %s, %s, %s) ', (session['id'], friend_id, chat_text, timestamp  ),)
             mysql.connection.commit()
-            cursor.execute('SELECT * FROM accounts where id = %s',   [friend_id])
-            account = cursor.fetchone()
-            cursor.execute('SELECT user1.name, user2.name,  message_text, message_date FROM dialogs   \
-            left join accounts as user1 on dialogs.message_from  = user1.id \
-            left join accounts as user2 on dialogs.message_to  = user2.id \
-            where  dialogs.message_from = %s or dialogs.message_to = %s  ;',   (session['id'],session['id']))
-       
-            dialog = cursor.fetchall()
-            return render_template('displayfriend.html', account=account, dialog=dialog)
+
+            return redirect(url_for('displayfriend'))
+
+
+        friend_id = request.form.get('friend_id')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts where id = %s',   [friend_id])
+        account = cursor.fetchone()
+        cursor.execute('SELECT user1.name, user2.name,  message_text, message_date FROM dialogs   \
+        left join accounts as user1 on dialogs.message_from  = user1.id \
+        left join accounts as user2 on dialogs.message_to  = user2.id \
+        where  dialogs.message_from = %s or dialogs.message_to = %s  ;',   (session['id'],session['id']))
+        dialog = cursor.fetchall()
+        return render_template('displayfriend.html', account=account, dialog=dialog, form=form )
+        
     return redirect(url_for('login'))
 
 @app.route('/social/addfriend', methods=['POST'])
